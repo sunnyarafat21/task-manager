72fa53040485a1312709b29fcb01b54a0210801d
unknown <yasirarafat0421@gmail.com>
2023-02-11 19:27:04 +0600

    change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app=require("./app");
+import("./.env")
 
-
-const PORT = 8080 ;
+const PORT = process.env.PORT || 8080 ;
 app.listen(PORT,function () {
     console.log(`App Run @${PORT}`)
 })
